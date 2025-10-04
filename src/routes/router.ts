2d97e39306c6bc2658c28cc4c5dc347169ef3d00
AktherHosen <dev.aktherhosen@gmail.com>
2025-10-04 15:53:56 +0600

    feat: added not found page

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,13 @@
 import Main from "@/layout/main";
 import Home from "@/pages/home/home";
+import NotFound from "@/pages/notFound/not-found";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Main,
+    ErrorBoundary: NotFound,
     children: [{ index: true, Component: Home }],
   },
 ]);
