508065fe2f04355e4278a0d26ee231f232b7d124
AktherHosen <dev.aktherhosen@gmail.com>
2025-10-04 15:41:49 +0600

    feat: added redux and redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import "./index.css";
 import { router } from "./routes/router";
-
+import { Provider } from 'react-redux'
+import { store } from "./redux/store";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
