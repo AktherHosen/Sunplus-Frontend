d9ec5b521be41b7571de04d8e2a8329878960799
AktherHosen <dev.aktherhosen@gmail.com>
2025-11-07 11:51:27 +0600

    fix: banner

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -14,28 +14,27 @@ export default function Banner() {
   return (
     <Carousel
       className="w-full overflow-hidden rounded-lg "
-      plugins={[Autoplay({ delay: 4000, stopOnInteraction: false })]}
-    >
-      <CarouselContent className="flex w-full ml-0 gap-0">
+      plugins={[Autoplay({ delay: 4000, stopOnInteraction: false })]}>
+      <CarouselContent className="flex w-full  ml-0 gap-0">
         <CarouselItem className="flex-shrink-0 w-full ml-0 pl-0">
           <img
             src={Car1}
             alt="Banner 1"
-            className="w-full max-h-[400px] object-cover rounded-lg m-0"
+            className="w-full max:h-[400px] object-cover rounded-lg transition-transform duration-700 ease-in-out"
           />
         </CarouselItem>
         <CarouselItem className="flex-shrink-0 w-full pl-0 ml-0">
           <img
             src={Car2}
             alt="Banner 2"
-            className="w-full max-h-[400px] object-cover rounded-lg m-0"
+            className="w-full max:h-[400px] object-cover rounded-lg transition-transform duration-700 ease-in-out"
           />
         </CarouselItem>
         <CarouselItem className="flex-shrink-0 w-full pl-0 ml-0">
           <img
             src={Car3}
             alt="Banner 3"
-            className="w-full max-h-[400px] object-cover rounded-lg m-0"
+            className="w-full max:h-[400px] object-cover rounded-lg transition-transform duration-700 ease-in-out"
           />
         </CarouselItem>
       </CarouselContent>
